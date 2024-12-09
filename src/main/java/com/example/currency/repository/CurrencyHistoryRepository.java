ffa4e3753935c6646494fd3f6bd6d804eff491b1
FoxRLY <nevik0016@gmail.com>
2024-12-09 17:43:29 +0300

    Урбал ненужные аннотации и добавли версионирование библиотек

--- src/main/java/com/example/currency/repository/CurrencyHistoryRepository.java
+++ src/main/java/com/example/currency/repository/CurrencyHistoryRepository.java
@@ -4,9 +4,7 @@ import com.example.currency.entity.CurrencyHistory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CurrencyHistoryRepository extends CrudRepository<CurrencyHistory, Long> {
 
   Page<CurrencyHistory> getByCurrencyId(Long currencyId, Pageable pageable);
--- src/main/java/com/example/currency/repository/CurrencyRepository.java
+++ src/main/java/com/example/currency/repository/CurrencyRepository.java
@@ -5,9 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CurrencyRepository extends CrudRepository<Currency, Long> {
 
   List<Currency> findAll();
